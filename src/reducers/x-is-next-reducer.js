5bad943961835723bcd0a542d24e399e6764fbf3
Student <example@example.com>
2018-09-20 14:18:01 -0700

    update reducers and tests, include root reducer, refactor game component, get everything working

--- src/reducers/x-is-next-reducer.js
+++ src/reducers/x-is-next-reducer.js
@@ -1,12 +1,8 @@
-export default (state = {xIsNext: true}, action) => {
+export default (state = true, action) => {
   let newState;
-  console.log(action);
   switch(action.type){
     case 'NEXT_TURN':
-      let newState = Object.assign({}, state, {
-        xIsNext: action.stepNumber % 2 === 0
-      });
-      return newState;
+      return action.stepNumber % 2 === 0;
     default:
       return state;
   }
