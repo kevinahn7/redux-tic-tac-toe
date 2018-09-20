3232871b117795d060ddb77788638fd99b18fbe4
Student <example@example.com>
2018-09-20 13:24:49 -0700

    import provider, create store and add components to file

--- src/index.js
+++ src/index.js
@@ -2,7 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Game from './components/Game';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import xIsNextReducer from './reducers/x-is-next-reducer';
 
-ReactDOM.render(<Game />, document.getElementById('root'));
+const store = createStore(xIsNextReducer)
 
+let unsubscribe = store.subscribe(() =>
+  console.log(store.getState())
+);
 
+ReactDOM.render(
+  <Provider store={store}>
+    <Game />
+  </Provider>,
+  document.getElementById('root'));
