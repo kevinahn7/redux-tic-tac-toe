f5427aee09313c7757eba8ebeebbe78acdd7d1fa
Student <example@example.com>
2018-09-20 13:25:12 -0700

    create reducers to handle state

---
+++ src/reducers/history-reducer.js
@@ -0,0 +1,8 @@
+export default (state = [{squares: Array(9).fill(null)}], action) => {
+  // let newSate;
+  // switch(action.type){
+  //   case 'CLICK_SQUARE':
+  //   newState =
+  // }
+  return state;
+}
