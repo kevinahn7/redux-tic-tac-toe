ff8024cdccc7761225516f5805f551a160358486
Student <example@example.com>
2018-09-20 16:18:50 -0700

    update history reducer to mutate state, dispatch action in game component

--- __tests__/reducers/history-reducer.test.js
+++ __tests__/reducers/history-reducer.test.js
@@ -4,5 +4,9 @@ describe("historyReducer", () => {
 
   test("Should return default state if no action type is recognized", () => {
     expect(historyReducer([{squares: Array(9).fill(null)}], { type: null })).toEqual([{squares: Array(9).fill(null)}]);
+  });
+
+  test('Should concat the next squares object to the history array', () => {
+    expect(historyReducer(history.length, { type: 'UPDATE_HISTORY' })).toEqual([{squares: Array(9).fill(null)}, {squares: Array(9).fill(null)}])
   })
 })
